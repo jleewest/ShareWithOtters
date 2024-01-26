a76ea8045665d26b71df407691d00c4d893225b0
GGar99 <giovannygarcia1999@gmail.com>
2024-01-26 12:12:57 -0700

    completed the basic componenet structure with placeholder info

---
+++ client/src/components/GroupOptions.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const GroupOptions = () => {
+  return(
+    <div>
+      <h1>Group Logo</h1>
+      <div>
+        <button>Add expense</button>
+        <button>Add payment</button>
+        <button>Invite friends</button>
+      </div>
+    </div>
+  )
+}
+export default GroupOptions;
