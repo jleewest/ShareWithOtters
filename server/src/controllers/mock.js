129050854aa1c6047714db50b6a38f8b840915aa
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-27 14:57:35 -0800

    fix: getTransactions properly gets and sorts transactions

--- server/src/controllers/mock.js
+++ server/src/controllers/mock.js
@@ -48,14 +48,14 @@
 		"description": "Hotel",
     "amount":[100],
 
-		"type": "income",
+		"type": "payment",
 		"date": "2024-01-19",
 		"transactor": "clerkid1",
 		"transactee": ["clerkid2"],
 		"description": "Hotel",
     "amount": [200],
 
-		"type": "income",
+		"type": "payment",
 		"date": "2024-01-19",
 		"transactor": "clerkid1",
 		"transactee": ["clerkid1", "clerkid2"],
