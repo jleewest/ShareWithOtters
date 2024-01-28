ce80b2246ef1c0b9975c8e828e96ef493af176db
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-28 11:49:31 -0800

    style: removed logo from header

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,12 +1,10 @@
 import Dashboard from './Dashboard';
 import '../css/Home.css';
-import logo from '../assets/otter-logo.svg';
 
 function Home() {
   return (
     <div className='Home'>
       <header className='app-header'>
-        <img src={logo} alt='logo' />
         <div>OtterShare</div>
       </header>
       <main className='home-container'>
