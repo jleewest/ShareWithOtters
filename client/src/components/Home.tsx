374b904cfd413bf90fd8cfec2de369d33922e8fa
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-28 11:43:10 -0800

    style home component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,12 +1,20 @@
 import Dashboard from './Dashboard';
 import '../css/Home.css';
+import logo from '../assets/otter-logo.svg';
 
 function Home() {
   return (
     <div className='Home'>
-      <header>OtterShare</header>
-      <Dashboard />
-      <footer>Open source code</footer>
+      <header className='app-header'>
+        <img src={logo} alt='logo' />
+        <div>OtterShare</div>
+      </header>
+      <main className='home-container'>
+        <Dashboard />
+      </main>
+      <footer className='app-footer'>
+        <a href='https://github.com/jleewest/IOU.git'>Open source code</a>
+      </footer>
     </div>
   );
 }
