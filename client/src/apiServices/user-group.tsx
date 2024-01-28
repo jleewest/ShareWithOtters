417caffb446f491ab187f04f9ac4408cb40c99b9
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-27 17:52:16 -0800

    feat: add user-group apiservice

---
+++ client/src/apiServices/user-group.tsx
@@ -0,0 +1,46 @@
+import { User_Group } from '../index';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export async function getGroupsByClerkId(id: string): Promise<User_Group[]> {
+  const response = await fetch(`${BASE_URL}/user-group/${id}`);
+  if (response.ok) {
+    const data = await response.json();
+    return data as Promise<User_Group[]>;
+  }
+  return Promise.reject(new Error('Something went 🦖RA-WRong'));
+}
+
+export async function addUserToGroup(body: User_Group): Promise<User_Group> {
+  const response = await fetch(`${BASE_URL}/user-group/`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      userId: body.userId,
+      groupId: body.groupId,
+    }),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data as Promise<User_Group>;
+  }
+  return Promise.reject(new Error('Something went 🦖RA-WRong'));
+}
+
+export async function deleteUserFromGroup(
+  body: User_Group
+): Promise<User_Group> {
+  const response = await fetch(`${BASE_URL}/user-group/delete`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      userId: body.userId,
+      groupId: body.groupId,
+    }),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data as Promise<User_Group>;
+  }
+  return Promise.reject(new Error('Something went 🦖RA-WRong'));
+}
