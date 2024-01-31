817acbe38980f01df73e06f534f25b931299d418
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-31 10:57:15 -0800

    initial LendingSummary commit

--- client/src/components/LendingSummary.tsx
+++ client/src/components/LendingSummary.tsx
@@ -1,4 +1,9 @@
+import { useTransactionContext } from '../index';
+
 const LendingSummary = () => {
+  const { transactions } = useTransactionContext();
+
+  console.log(transactions);
   return (
     <div className='LendingSummary'>
       <h1>LendingSummary Render Here</h1>;
