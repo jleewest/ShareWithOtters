e17641d1af38ce1d951e1834905583b287312844
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-02-01 17:31:15 -0800

    feat: waveChart sorts transactions into groups by date of transaction

--- client/src/components/WaveChart.tsx
+++ client/src/components/WaveChart.tsx
@@ -1,4 +1,68 @@
+import { TransactionWithUser } from '../index';
+import { useTransactionContext } from '../index';
+import { useState, useEffect } from 'react';
+import { useUser } from '@clerk/clerk-react';
+
 const WaveChart = () => {
+  const { transactions } = useTransactionContext();
+  const [groupedTransactions, setGroupedTransactions] = useState<
+    TransactionWithUser[][]
+  >([]);
+  const { user } = useUser();
+
+  ////Group transactions by day
+  useEffect(() => {
+    if (transactions) {
+      const allTransactions = [
+        ...transactions.pending.expense,
+        ...transactions.pending.payment,
+        ...transactions.active.expense.confirmedExpenses,
+        ...transactions.active.payment.received,
+        ...transactions.active.expense.awaitedPendingExpenseSentToOther,
+        ...transactions.active.payment.paid,
+        ...transactions.active.payment.pendingPaid,
+      ];
+
+      //sort transactions by date
+      const sortedTransactions = allTransactions
+        .filter(
+          (transaction) => transaction.transactor !== transaction.transactee
+        )
+        .sort((a, b) => a.date.localeCompare(b.date));
+
+      //create array of date arrays containing respective transactions
+      const groupedTransactions: TransactionWithUser[][] = [];
+      sortedTransactions.forEach((transaction) => {
+        const transactionDate = new Date(transaction.date).toLocaleDateString();
+        console.log(transactionDate);
+        let existingArray = groupedTransactions.find(
+          (existingDateGroup) =>
+            new Date(existingDateGroup[0].date).toLocaleDateString() ===
+            transactionDate
+        );
+        if (!existingArray) {
+          existingArray = [];
+          groupedTransactions.push(existingArray);
+        }
+        existingArray.push(transaction);
+      });
+      setGroupedTransactions(groupedTransactions);
+    }
+  }, [transactions]);
+
+  console.log(groupedTransactions);
+
+  // Group transactions by month and calculate total lent and owed
+  //export const groupByMonth = (transactions: TransactionReturn) => {
+  //  return transactions.reduce((acc, transaction) => {
+  //    const month = new Date(transaction.date).getMonth();
+  //    if (!acc[month]) acc[month] = 0;
+  //    // Use absolute value of amount
+  //    acc[month] += Math.abs(transaction.amount);
+  //    return acc;
+  //  }, {} as Record<number, number>);
+  //};
+
   return (
     <div
       className='WaveChart'
