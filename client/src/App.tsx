d11abc535d2d3f94081b923c4333c5b888e24cf0
GGar99 <giovannygarcia1999@gmail.com>
2024-02-02 17:33:07 -0700

    Working Dark Mode toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import Home from './components/Home';
 import './css/App.css';
+import DarkModeToggle from './css/DarkModeToggle';
 import { SignedOut, SignedIn, SignInButton } from '@clerk/clerk-react';
 import logo from './assets/otter-logo.svg';
 
 function App() {
   return (
     <div className='App'>
+      <DarkModeToggle/>
       <SignedOut>
         <div className='signed-out'>
           <h1>OtterShare</h1>
---
+++ client/src/css/DarkModeToggle.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import sun from '../assets/sun.svg'; // Ensure the path is correct
+import moon from '../assets/moon.svg'; // Ensure the path is correct
+
+const DarkModeToggle: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    darkMode ? root.classList.add('dark') : root.classList.remove('dark');
+  }, [darkMode]);
+
+  return (
+    <button className="dark-mode-toggle" onClick={() => setDarkMode(!darkMode)}>
+      <img src={darkMode ? sun : moon} alt={darkMode ? "Light Mode" : "Dark Mode"} />
+    </button>
+  );
+};
+
+export default DarkModeToggle;
