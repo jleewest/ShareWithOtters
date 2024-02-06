3497fab645f4c14d18e1070d293b57695221c706
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-02-06 10:18:13 -0800

    refactor: groupDetails to named export

--- client/src/components/GroupDetails.tsx
+++ client/src/components/GroupDetails.tsx
@@ -1,18 +1,18 @@
 import '../css/GroupDetails.css';
 import logo from '../assets/otter-logo.svg';
 
-type groupProps = { group: { title: string; description: string } };
+type Group = { title: string; description: string };
 
-const GroupDetails = (group: groupProps) => {
+type GroupDetailsProps = { group: Group };
+
+export const GroupDetails: React.FC<GroupDetailsProps> = (props) => {
   return (
     <div className='GroupDetails'>
       <img className='login-logo group-img' src={logo} alt='logo' />
       <div className='group-details'>
-        <div className='group-name'>{group.group.title}</div>
-        <div className='group-description'>{group.group.description}</div>
+        <div className='group-name'>{props.group.title}</div>
+        <div className='group-description'>{props.group.description}</div>
       </div>
     </div>
   );
 };
-
-export default GroupDetails;
