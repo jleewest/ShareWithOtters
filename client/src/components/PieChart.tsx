7f34773b91bc98f438813ba52fc0b4de7ffb862f
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-02-06 06:21:36 -0800

    feat: pie chart specific to group

--- client/src/components/PieChart.tsx
+++ client/src/components/PieChart.tsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData } from 'chart.js';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  ChartData,
+} from 'chart.js';
 import { getAllUsers } from '../apiServices/user';
 import { getTransactionsByClerkId } from '../apiServices/transaction';
 import '../css/PieChart.css';
-import { chartBackgroundColors, chartHoverBackgroundColors } from '../css/PieChartColors';
+import {
+  chartBackgroundColors,
+  chartHoverBackgroundColors,
+} from '../css/PieChartColors';
+import { useParams } from 'react-router-dom';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart: React.FC = () => {
-  const [chartData, setChartData] = useState<ChartData<'pie', number[], string>>({
+  const [chartData, setChartData] = useState<
+    ChartData<'pie', number[], string>
+  >({
     labels: [],
     datasets: [
       {
@@ -20,24 +32,33 @@ const PieChart: React.FC = () => {
     ],
   });
   const [showAmount, setShowAmount] = useState<boolean>(true); // State variable for toggling
-
+  const params = useParams();
   const fetchData = async () => {
     const users = await getAllUsers();
     const userExpenses: { [key: string]: number } = {};
 
     for (const user of users) {
-      const transactions = await getTransactionsByClerkId(user.clerkId);
-      userExpenses[user.firstName] = transactions.active.expense.confirmedExpenses.reduce((acc, curr) => showAmount ? acc + curr.amount : acc + 1, 0);
+      const transactions = await getTransactionsByClerkId(
+        user.clerkId,
+        params.id
+      );
+      userExpenses[user.firstName] =
+        transactions.active.expense.confirmedExpenses.reduce(
+          (acc, curr) => (showAmount ? acc + curr.amount : acc + 1),
+          0
+        );
     }
 
     // Update chartData state
     setChartData({
       labels: Object.keys(userExpenses),
-      datasets: [{
-        data: Object.values(userExpenses),
-        backgroundColor: chartBackgroundColors,
-        hoverBackgroundColor: chartHoverBackgroundColors,
-      }],
+      datasets: [
+        {
+          data: Object.values(userExpenses),
+          backgroundColor: chartBackgroundColors,
+          hoverBackgroundColor: chartHoverBackgroundColors,
+        },
+      ],
     });
   };
 
@@ -46,10 +67,13 @@ const PieChart: React.FC = () => {
   }, [showAmount]);
 
   return (
-    <div className="pie-chart-container">
+    <div className='pie-chart-container'>
       <h2>Who's Covering Otters</h2>
-      <button onClick={() => setShowAmount(!showAmount)} className="pie-chart-toggle-btn">
-        {showAmount ? 'Total Amount' : 'How Many Expenses' }
+      <button
+        onClick={() => setShowAmount(!showAmount)}
+        className='pie-chart-toggle-btn'
+      >
+        {showAmount ? 'Total Amount' : 'How Many Expenses'}
       </button>
       <Pie data={chartData} />
     </div>
